2beff23e1f33f63783255aacfcaf86cb170e8bc1
Mikhail Scheglov <mvscheglov@avito.ru>
2023-11-21 04:43:03 +0300

    Add test case

--- internal/solver/solver_test.go
+++ internal/solver/solver_test.go
@@ -1,7 +1,9 @@
 package solver
 
 import (
+	"encoding/binary"
 	"github.com/stretchr/testify/assert"
+	"math"
 	"testing"
 )
 
@@ -28,7 +30,18 @@ func TestSolver_1Root(t *testing.T) {
 }
 
 func TestSolver_ZeroCoefficient(t *testing.T) {
-	solver := NewSolver(defaultEps)
+	solver := NewSolver(0.01)
 	_, err := solver.Solve(0, 1, 1)
 	assert.ErrorIs(t, err, errZeroCoefficient)
+
+	_, err = solver.Solve(0.001, 1, 1)
+	assert.ErrorIs(t, err, errZeroCoefficient)
+}
+
+// Not very necessary test case because we doesn't matter what float64 values Solve() will get
+func TestSolver_Corrupted(t *testing.T) {
+	solver := NewSolver(defaultEps)
+	res, err := solver.Solve(math.Float64frombits(binary.LittleEndian.Uint64([]byte{0, 0, 0, 0, 0, 0, 240, 63})), -2, 1)
+	assert.NoError(t, err)
+	assert.Equal(t, []float64{1}, res)
 }
